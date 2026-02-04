1c9a0f9eb086f66e3075ff29850033783f9c0d1b
bharat948 <bharatpatidar.002@gmail.com>
2026-02-04 14:43:53 +0530

    fix: resolve duplicate import and update db state

--- src/services/topicEngine.js
+++ src/services/topicEngine.js
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import * as jsonStore from '../store/jsonStore.js';
 import { cosineSimilarity, averageEmbeddings } from '../utils/similarity.js';
-import { cosineSimilarity, averageEmbeddings } from '../utils/similarity.js';
 import { generateTitleAndSummary } from './topicSummarizer.js';
 import { embed } from './embedder.js';
 
